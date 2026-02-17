fead9ed6986d4590dc3b83d9eb43c9ef9d7a6abb
rabtamarket-dot <rabtamarket@gmail.com>
2026-02-17 13:05:22 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './')
+    }
   },
   build: {
     outDir: 'dist',
